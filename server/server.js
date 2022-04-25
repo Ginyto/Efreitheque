b60c1c965435bf041b35c9c893b6ef85f8e6748d
Ginyto <agianlucca@gmail.com>
2022-04-25 14:55:03 +0200

    done

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenu à l'efreitheque" });
+});
+
+// set port, listen for requests
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
